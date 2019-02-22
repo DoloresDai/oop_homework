9bcf11dbcaab12d6dbbd47f3349f41eff1c5a2c0
DoloresDai <841856753@qq.com>
2019-02-22 19:39:58 +0800

    Create a Person class with name, age, id, and corresponding behavior.

---
+++ src/main/java/dai/model/Person.java
@@ -0,0 +1,49 @@
+package main.java.dai.model;
+
+public class Person {
+    private String name;
+    private int age;
+    private String id;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person(String name, int age) {
+        this(name);
+        this.age = age;
+    }
+
+    public Person(String name, int age, String id) {
+        this(name, age);
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String introduce() {
+        return "My namn is " + this.name + ".I am " + this.age + " years old.";
+    }
+}
