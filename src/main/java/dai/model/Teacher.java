76c8a93f6e438b8511b6fc5452daaf5b7da08b17
DoloresDai <841856753@qq.com>
2019-03-02 16:27:48 +0800

    Improve teacher function of isTeaching.

--- src/main/java/dai/model/Teacher.java
+++ src/main/java/dai/model/Teacher.java
@@ -4,52 +4,33 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Teacher extends Person {
-    private String name;
-    private int age;
-    private String id;
-    private int[] classess;
+    private int[] classes;
 
-    public Teacher(String name) {
-        super(name);
-    }
-
-    public Teacher(String name, int age) {
-        super(name, age);
-    }
-
-    public Teacher(String name, int age, String id) {
+    public Teacher(String name, int age, String id,int[] classes) {
         super(name, age, id);
+        this.classes=classes;
     }
 
-    public Teacher(String name, int age, String id, int[] classess) {
-        super(name, age, id);
-        this.classess = classess;
+    public int[] getClasses() {
+        return classes;
     }
 
-    public int[] getClassess() {
-        return classess;
-    }
-
-    public void setClassess(int[] classess) {
-        this.classess = classess;
+    public void setClasses(int[] classes) {
+        this.classes = classes;
     }
 
     @Override
     public String introduce() {
-        if (getClassess().length == 0) {
+        if (getClasses().length == 0) {
             return super.introduce() + "I am a Teacher. I teach No Class.";
         } else {
-            return super.introduce() + "I am a Teacher. I teach Class " + Arrays.toString(classess);
+            return super.introduce() + "I am a Teacher. I teach Class " + Arrays.toString(classes);
         }
     }
 
-    public boolean isTeaching(Student student) {
-//           System.out.println(IntStream.range(0, classess.length).filter(i -> student.getClass().equals(i)));
-        for (int elements : classess) {
-            if (elements == (student.getaClass())) {
-                return true;
-            } else return false;
+    public boolean isTeaching(Class aclass,Student student) {
+        return aclass.isIn(student,this);
         }
     }
-}
+
 
